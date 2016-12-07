76ce2ffc61d4224df9abe306c7f2d73bb9184f3a
Oleg Slobodin <olegslobodin@mail.ru>
2016-12-07 18:46:18 +0300

    Add project files.

---
+++ Canteen/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Canteen.Startup))]
+namespace Canteen
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}

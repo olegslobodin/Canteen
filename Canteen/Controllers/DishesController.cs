373383a54b96fe360a10462db4c397571f80fea1
Oleg Slobodin <olegslobodin@mail.ru>
2016-12-07 19:07:38 +0300

    Added dishes controller.

---
+++ Canteen/Controllers/DishesController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Canteen.Models;
+
+namespace Canteen.Controllers
+{
+    public class DishesController : Controller
+    {
+        private CanteenEntities db = new CanteenEntities();
+
+        // GET: Dishes
+        public ActionResult Index()
+        {
+            var dishes = db.Dishes.Include(d => d.Worker);
+            return View(dishes.ToList());
+        }
+
+        // GET: Dishes/Details/5
+        public ActionResult Details(long? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Dish dish = db.Dishes.Find(id);
+            if (dish == null)
+            {
+                return HttpNotFound();
+            }
+            return View(dish);
+        }
+
+        // GET: Dishes/Create
+        public ActionResult Create()
+        {
+            ViewBag.WorkerId = new SelectList(db.Workers, "Id", "FirstName");
+            return View();
+        }
+
+        // POST: Dishes/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Title,Price,WorkerId")] Dish dish)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Dishes.Add(dish);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.WorkerId = new SelectList(db.Workers, "Id", "FirstName", dish.WorkerId);
+            return View(dish);
+        }
+
+        // GET: Dishes/Edit/5
+        public ActionResult Edit(long? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Dish dish = db.Dishes.Find(id);
+            if (dish == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.WorkerId = new SelectList(db.Workers, "Id", "FirstName", dish.WorkerId);
+            return View(dish);
+        }
+
+        // POST: Dishes/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Title,Price,WorkerId")] Dish dish)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(dish).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.WorkerId = new SelectList(db.Workers, "Id", "FirstName", dish.WorkerId);
+            return View(dish);
+        }
+
+        // GET: Dishes/Delete/5
+        public ActionResult Delete(long? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Dish dish = db.Dishes.Find(id);
+            if (dish == null)
+            {
+                return HttpNotFound();
+            }
+            return View(dish);
+        }
+
+        // POST: Dishes/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(long id)
+        {
+            Dish dish = db.Dishes.Find(id);
+            db.Dishes.Remove(dish);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
